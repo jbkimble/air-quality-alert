aa4b96f3c7b23039b55805f35838fa6421bd7b68
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-05 10:45:22 -0700

    Add test for get_rating method

--- app/models/weather_point.rb
+++ app/models/weather_point.rb
@@ -1,3 +1,22 @@
 class WeatherPoint < ApplicationRecord
-  validates :city, :state, :country, :zipcode, :airquality, :rating, presence: true
+  validates :city, :state, :country, :zipcode, :aqi, :rating, presence: true
+
+  def self.get_rating(aqi)
+    if 0 <= aqi && aqi <= 50
+      return "Good"
+    elsif 51 <= aqi && aqi <= 100
+      return "Moderate"
+    elsif 101 <= aqi && aqi <= 150
+      return "Unhealthy for Sensitive Groups"
+    elsif 151 <= aqi && aqi <= 200
+      return "Unhealthy"
+    elsif 201 <= aqi && aqi <= 300
+      return "Very Unhealthy"
+    elsif 301 <= aqi && aqi <= 500
+      return "Hazardous"
+    else
+      return "Invalid AQI"
+    end
+  end
+
 end

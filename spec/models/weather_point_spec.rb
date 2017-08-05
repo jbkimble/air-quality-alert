02552512b918c480875ed1394a4788e103242ffc
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-05 10:46:34 -0700

    Add test for weather_point_spec.rb

--- spec/models/weather_point_spec.rb
+++ spec/models/weather_point_spec.rb
@@ -30,14 +30,38 @@ describe WeatherPoint do
       expect(measurement).to be_invalid
     end
 
-    it 'is invalid without airquality' do
-      measurement = Fabricate.build(:weatherPoint, airquality: nil)
+    it 'is invalid without aqi' do
+      measurement = Fabricate.build(:weatherPoint, aqi: nil)
       expect(measurement).to be_invalid
     end
 
     it 'is invalid without rating' do
       measurement = Fabricate.build(:weatherPoint, rating: nil)
       expect(measurement).to be_invalid
+    end
+  end
+
+  context 'test methods' do
+    scenario 'get_rating method returns proper rating' do
+      rating_good = WeatherPoint.get_rating(5)
+      rating_moderate = WeatherPoint.get_rating(51)
+      rating_unhealthy_1 = WeatherPoint.get_rating(121)
+      rating_unhealthy_11 = WeatherPoint.get_rating(150)
+      rating_unhealthy_2 = WeatherPoint.get_rating(151)
+      rating_very_unhealthy = WeatherPoint.get_rating(222)
+      rating_hazardous = WeatherPoint.get_rating(330)
+      invalid_1 = WeatherPoint.get_rating(501)
+      invalid_2 = WeatherPoint.get_rating(-1)
+
+      expect(rating_good).to eq("Good")
+      expect(rating_moderate).to eq("Moderate")
+      expect(rating_unhealthy_1).to eq("Unhealthy for Sensitive Groups")
+      expect(rating_unhealthy_11).to eq("Unhealthy for Sensitive Groups")
+      expect(rating_unhealthy_2).to eq("Unhealthy")
+      expect(rating_very_unhealthy).to eq("Very Unhealthy")
+      expect(rating_hazardous).to eq("Hazardous")
+      expect(invalid_1).to eq("Invalid AQI")
+      expect(invalid_2).to eq("Invalid AQI")
     end
   end
 end

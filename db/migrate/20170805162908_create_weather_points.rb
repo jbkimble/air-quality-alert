5dc42cff3ff16f47c835d074212f5b2c583150df
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-05 10:05:17 -0700

    Add weatherpoints table

---
+++ db/migrate/20170805162908_create_weather_points.rb
@@ -0,0 +1,14 @@
+class CreateWeatherPoints < ActiveRecord::Migration[5.0]
+  def change
+    create_table :weather_points do |t|
+      t.string :city
+      t.string :state
+      t.string :country
+      t.integer :zipcode
+      t.string :airquality
+      t.string :rating
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170805162908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "weather_points", force: :cascade do |t|
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.integer  "zipcode"
+    t.string   "airquality"
+    t.string   "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

3fb50f043b814ff1b5132d2d4fb281aeb265f0e6
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-05 11:15:43 -0700

    Remove comments

--- app/controllers/airquality_controller.rb
+++ app/controllers/airquality_controller.rb
@@ -26,28 +26,9 @@ class AirqualityController < ApplicationController
     if wp.save
       redirect_to airquality_path(wp.id)
     else
-      #sad path :'(
+      redirect_to root_path
     end
-    #add 'get_rating method which takes aqi and return us government text rating'
-    #build weatherpoint object
-    #redirect to that objects show page and display information
-    # redirect_to airquality_path
+
   end
-
-  # 1. Person enters valid zipcode into text field
-  # 2. They press 'Get Air Quality'
-  # 3. post is sent to AirqualityController
-  # 4. AirqualityController create method sends zipcode to ZipcodeAPI and gets
-  #   return lat long coordinates.
-  # 5. Send return lat long coordinates to AirVisualAPI and gets return of
-  #   JSON
-  # 6. Parse Json and get Air Quality Index and city name.
-  # 7. Create entry in the database of datapoint(city, state, country, airquality,
-  #                                             AQI rating, created_at, updated_atd)
-  # 8. Redirect to airquality_show_path and show the object that was just created.
-  #
-  # End. They see the air quality for the nearest station to the zipcode they
-  #      entered displayed on the page.
-
 
 end

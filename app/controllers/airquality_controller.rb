9438948e0aa53831977ed8cdcb13d61e4b853048
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-05 11:03:30 -0700

    Pass to show view and display data

--- app/controllers/airquality_controller.rb
+++ app/controllers/airquality_controller.rb
@@ -2,6 +2,10 @@ class AirqualityController < ApplicationController
   def index
   end
 
+  def show
+    @weatherpoint = WeatherPoint.find(params["id"])
+  end
+
   def create
     uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params["zipcode"]}&key=#{ENV["google_key"]}")
     response = Net::HTTP.get(uri)
@@ -16,12 +20,18 @@ class AirqualityController < ApplicationController
     city_name = air_quality_data[:data][:city]
     state_name = air_quality_data[:data][:state]
     country_name = air_quality_data[:data][:country]
-    WeatherPoint.new(city: city_name, state: state_name, country: country_name,
-                    zipcode: params["zipcode"], aqi: us_aqi, rating:"good")
+    wp = WeatherPoint.new(city: city_name, state: state_name, country: country_name,
+                    zipcode: params["zipcode"], aqi: us_aqi, rating:WeatherPoint.get_rating(us_aqi))
+
+    if wp.save
+      redirect_to airquality_path(wp.id)
+    else
+      #sad path :'(
+    end
     #add 'get_rating method which takes aqi and return us government text rating'
     #build weatherpoint object
     #redirect to that objects show page and display information
-    redirect_to airquality_path
+    # redirect_to airquality_path
   end
 
   # 1. Person enters valid zipcode into text field

3662433fac363fbe7c352ec83e9140c8bd140fec
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-05 10:03:04 -0700

    Add fabricator for weatherPoint model

---
+++ spec/fabricators/weatherpoint_fabricator.rb
@@ -0,0 +1,8 @@
+Fabricator(:weatherPoint) do
+  city Faker::Address.city
+  state Faker::Address.state
+  country Faker::Address.country
+  zipcode Faker::Address.zip_code
+  airquality Faker::Number.between(1, 200)
+  rating Faker::Number.digit
+end

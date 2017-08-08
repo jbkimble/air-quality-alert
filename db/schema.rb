8386c5d880a9cf1ee6afba3ec2f98741bd1c7f2c
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-08 14:50:48 -0700

    Update column named aqi to us_aqi

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805162908) do
+ActiveRecord::Schema.define(version: 20170808214725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,13 @@ ActiveRecord::Schema.define(version: 20170805162908) do
     t.string   "state"
     t.string   "country"
     t.integer  "zipcode"
-    t.string   "aqi"
+    t.string   "us_aqi"
     t.string   "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "china_aqi"
+    t.string   "latitude"
+    t.string   "longitude"
   end
 
 end

edbb5bf125b12830a31e668605566dd2160c3f0e
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-08 15:01:50 -0700

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: "airquality#index", as: 'root'
-  get '/test', to: "welcome#index"
-  resources :airquality, only: [:create, :show]
+  resources :airquality, only: [:create]
 end

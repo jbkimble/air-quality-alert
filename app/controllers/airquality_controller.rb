3908c04596304e344da748456f1df8092a8a2a1c
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-09 18:11:43 -0700

    Convert aqi to integer before sending to model method

--- app/controllers/airquality_controller.rb
+++ app/controllers/airquality_controller.rb
@@ -5,7 +5,7 @@ class AirqualityController < ApplicationController
 
   def create
     local_air_data = AirDataService.new.get_air_quality(params["zipcode"])
-    local_air_data[:rating] = WeatherPoint.get_us_rating(local_air_data[:us_aqi])
+    local_air_data[:rating] = WeatherPoint.get_us_rating(local_air_data[:us_aqi].to_i)
     wp = WeatherPoint.new(local_air_data)
     if wp.save
       respond_to do |format|

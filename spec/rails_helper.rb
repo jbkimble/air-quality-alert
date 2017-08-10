66ac2247081288e76eac664332324ffbb3e6780e
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-10 13:54:46 -0700

    Add configs for selenium chrome

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,3 +55,14 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :selenium_chrome
+
+Capybara.configure do |config|
+  config.default_max_wait_time = 10 # seconds
+  config.default_driver        = :selenium
+end

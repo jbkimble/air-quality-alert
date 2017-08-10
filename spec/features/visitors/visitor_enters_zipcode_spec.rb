d519b828d008b721d7d8e58694c7537e1a15b5b4
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-10 13:55:44 -0700

    Add test for backup api

--- spec/features/visitors/visitor_enters_zipcode_spec.rb
+++ spec/features/visitors/visitor_enters_zipcode_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
-describe 'as a visitor' do
+describe 'as a visitor', js: true do
   scenario 'When I enter a valid zipcode I see data about that zipcode' do
     visit root_path
     fill_in 'zipcode', with: "98372"
     find_button('Get Air Quality').click
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Moderate")
+    expect(page).to have_content("Puyallup")
+  end
 
-    expect(current_path).to eq(air_quality_path)
-    expect(page).to have_content("140")
-    expect(page).to have_content("Unhealthy for Sensative Groups")
-    expect(page).to have_content("Seattle")
+  scenario 'When I enter a zipcode that air visual has no data for back up api returns info' do
+    visit root_path
+    fill_in 'zipcode', with: "98006"
+    find_button('Get Air Quality').click
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Unhealthy for Sensitive Groups")
+    expect(page).to have_content("Seattle-Bellevue-Kent Valley, USA")
   end
 end

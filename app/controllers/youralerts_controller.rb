8fb25d655b92567380c23ea19fed2fe4c10b47fb
Jon Kimble <jonathanbkimble@gmail.com>
2017-09-05 12:07:26 -0700

    Pass alert ID to client via json

--- app/controllers/youralerts_controller.rb
+++ app/controllers/youralerts_controller.rb
@@ -1,6 +1,6 @@
 class YouralertsController < ApplicationController
   def index
-    user_alerts = Alert.where(phone: alert_params["number"]).to_json(:include => {:air_index => {:only => :index}}, :only => :zipcode)
+    user_alerts = Alert.where(phone: alert_params["number"], active: true).to_json(:include => {:air_index => {:only => :index}}, :only => [:zipcode, :id])
     respond_to do |format|
       format.json { render json: user_alerts, :status => :ok}
     end

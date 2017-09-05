218ab013f51ec884161d57153d9dd16b4c114c0c
Jon Kimble <jonathanbkimble@gmail.com>
2017-09-05 15:06:26 -0700

    Add method to return stylized phone number

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,4 +2,9 @@ class Alert < ApplicationRecord
   validates :phone, :air_index_id, :zipcode, presence: true
   validates_inclusion_of :active, :in => [true, false]
   belongs_to :air_index
+
+  def styled_number
+    phone_number = self.phone
+    (phone_number[0..2] + "-" + phone_number[3..5] + "-" +  phone_number[6..9])
+  end
 end

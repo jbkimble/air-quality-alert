d3459edc8f91bfddb750578b4ab3fe7f5b200e37
Jon Kimble <jonathanbkimble@gmail.com>
2017-09-18 11:11:12 -0700

    Change alert service to reflect new architecture and be more resiliant to change

--- app/services/alert_service.rb
+++ app/services/alert_service.rb
@@ -5,13 +5,15 @@ class AlertService
     todays_rating_by_zip = []
     unique_zipcodes.each do |zipcode|
       zipcode_air_data = AirDataService.new.get_air_quality(zipcode)
-      WeatherPoint.create(zipcode_air_data)
-      todays_rating_by_zip << [zipcode, todays_air_rating]
+      todays_air_aqi = WeatherPoint.create(zipcode_air_data).us_aqi.to_i
+      todays_rating_by_zip << [zipcode, todays_air_aqi]
     end
-    todays_rating_by_zip.each do |zipcode, rating|
+    todays_rating_by_zip.each do |zipcode, aqi|
       alerts_for_zipcode = Alert.where(zipcode: zipcode, active: true)
       alerts_for_zipcode.each do |alert|
-        if alert.air_index_id <= rating
+        # check to see if it is above the bottom range of the index they wanted to be notified at
+        # and send alert if that is the case
+        if alert.air_index.bottom_range <= aqi
           TwilioService.send_alert(alert.phone, alert.zipcode, alert.air_index.index)
         end
       end
